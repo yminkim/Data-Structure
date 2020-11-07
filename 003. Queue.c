36a7dfb838d78789aecfd6eb84ad99549a13c588
KIM YOUNGMIN <dudals6116@naver.com>
2020-11-08 01:33:01 +0900

    enqueue, dequeue 구현

--- 003. Queue.c
+++ 003. Queue.c
@@ -18,18 +18,32 @@ void enqueue(Stack* *front, int data, Stack* *rear) {
 	temp = *front;
 	if (temp == 0) {
 		*front = get_node();
+		(*front)->data = data;
+		(*front)->link = 0;
 		*rear = *front;
-		(*rear)->data = data;
-		(*rear)->link = 0;
 	}
 	else {
 		temp = *rear;
 		*rear = get_node();
 		(*rear)->data = data;
 		(*rear)->link = 0;
-		(*temp).link = *rear;
+		temp->link = *rear;
+
 	}
-	
+
+}
+
+void print_stack(Stack* *front) {
+	Stack* temp = *front;
+	for (temp = *front; temp != NULL; temp = temp->link) {
+		printf("%d\n", temp->data);
+	}
+}
+
+void dequeue(Stack* *front) {
+	Stack* temp = *front;
+	*front = (*front)->link;
+	free(temp);
 }
 
 int main() {
@@ -39,5 +53,9 @@ int main() {
 	enqueue(&front, 10, &rear);
 	enqueue(&front, 20, &rear);
 	enqueue(&front, 30, &rear);
+	print_stack(&front);
+	dequeue(&front);
+	printf("-------\n");
+	print_stack(&front);
 
 }
