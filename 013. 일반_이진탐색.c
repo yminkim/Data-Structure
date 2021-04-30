d0e52fd539fa08fbd0e599cec1df71ac36ac1f90
KIM YOUNGMIN <dudals6116@naver.com>
2021-05-01 01:14:58 +0900

    Update 013. 일반_이진탐색.c

--- 013. 일반_이진탐색.c
+++ 013. 일반_이진탐색.c
@@ -9,34 +9,40 @@ int two(int arr[], int len, int target) {
 		mid = (first + last) / 2;
 		if (target == arr[mid])
 			return mid;
-		else {
-			if (target > arr[mid])
-				first = mid + 1;
-			else
-				last = mid - 1;
-		}
-
+		else if (target > arr[mid])
+			first = mid + 1;
+		else
+			last = mid - 1;
 	}
 	return -1;
-
 }
 
 
 int main() {
-	int arr[9] = {1,2,3,7,9,12,21,23,27};
-
-	
-	int len = sizeof(arr) / sizeof(arr[0]);
+	int arr[10] = {1, 3, 5, 8, 9, 11, 15, 18, 24, 40};
+	int len = sizeof(arr) / sizeof(int);
 	int target;
-	printf("배열값: ");
-	for (int i = 0; i < len; i++) {
-		printf("%d ", arr[i]);
+	
+	while (1) {
+		system("cls");
+		printf("배열: ");
+		for (int i = 0; i < len; i++)
+			printf("%d ", arr[i]);
+		printf("\n찾을 값을 입력: ");
+		scanf_s("%d", &target);
+		if (two(arr, len, target) == -1) {
+			printf("찾는 값이 없습니다.\n");
+			system("pause");
+		}
+		else {
+			printf("찾는 %d는 %d 번째 index입니다\n", target, two(arr, len, target));
+			system("pause");
+		}
+
+
+
 	}
-
-	printf("\n찾을 값: ");
-	scanf_s("%d", &target);
-
-	printf("찾는 값 %d인덱스인 %d", two(arr, len, target), arr[two(arr, len, target)]);
+	
 
 	return 0;
 }
